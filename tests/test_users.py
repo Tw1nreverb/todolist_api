bfcf3d53c3aeec7fdcc9380e2ee34e8b200c9088
Tw1nreverb <vladislavik259@gmail.com>
2024-09-20 17:54:51 +0400

    refactor test

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,11 +1,9 @@
 from domain.model import User
-from src.logic.hash import get_password_hash
-import bcrypt
+from src.logic.hash import get_password_hash, verify_password
 def test_user_hash_password():
     user = User(
-        name='Vlad',
         password='qwerty123',
         email='index@mail.ru',
     )
-    user_hashed_password = get_password_hash(user.get_password())
-    assert bcrypt.checkpw(user.get_password().encode(),user_hashed_password)
+    hashed_password = get_password_hash(user.password)
+    assert verify_password(user.password,hashed_password) 

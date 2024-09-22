d08a846d75538ae9db2cd5b6805acbbba1e4f17a
Vladislav Merkodanov <vladislavik259@gmail.com>
2024-09-22 21:44:19 +0400

    refactor test

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,5 +1,5 @@
 from src.domain.model import User
-from src.logic.service import register, add_user
+from src.logic.service import register, add_user, get_user_by_email
 from src.uow.user.uow import FakeUOW
 
 
@@ -8,12 +8,11 @@ async def test_register():
     assert await register(email='<EMAIL>', password='<PASSWORD>', uow=fake_uow) is True
 
 
-async def test_add_user():
+async def test_add_user_and_get_by_email():
     user = User(email='<EMAIL>', password='<PASSWORD>')
     fake_uow = FakeUOW()
     await add_user(uow=fake_uow, email=user.email, password=user.password)
     assert fake_uow.committed is True
 
-    expected_email = await fake_uow.users.get(email=user.email)
-    expected_email = expected_email.email
-    assert expected_email == user.email
+    expected_email = await get_user_by_email(user.email,uow=fake_uow)
+    assert expected_email.email == user.email
